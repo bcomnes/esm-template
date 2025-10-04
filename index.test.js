2eb8107052a87aba0781b05cb523660d7e088cb2
Bret Comnes <bcomnes@gmail.com>
2025-10-03 17:30:38 -0700

    Fix test

--- index.test.js
+++ index.test.js
@@ -2,6 +2,6 @@ import test from 'node:test'
 import assert from 'node:assert'
 import { hello } from './index.js'
 
-test('index export', async (t) => {
+test('index export', async (_t) => {
   assert.strictEqual(hello(), 'world', 'A message')
 })
